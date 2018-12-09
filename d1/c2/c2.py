49eed8a29ed4ec505be727a9e3b0118e95d30ece
Étienne Lévesque <etienne.levesque16@gmail.com>
2018-12-08 21:57:38 -0500

    Day 1, challenge 2

--- d1/c2/c2.py
+++ d1/c2/c2.py
@@ -4,5 +4,15 @@ import os
 filePath = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, 'input.txt')
 file = open(filePath, "r")
 inputs = [int(line) for line in file.read().split('\n')]
+file.close()
 
-print(reduce((lambda x, y: x + y), inputs))
+i = 0
+frequency = 0
+reachedFrequencies = []
+while not frequency in reachedFrequencies:
+    reachedFrequencies.append(frequency)
+    frequency += inputs[i]
+    i += 1
+    i %= len(inputs)
+
+print(frequency)
